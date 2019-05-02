089cd6164e2765fcafc126e6d4553f7517258bcb
dziem <dziemboh@gmail.com>
2019-05-02 11:03:02 +0700

    Change the con method

--- conn.js
+++ conn.js
@@ -1,14 +1,20 @@
 var mysql = require('mysql');
-
+/*
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "londri"
 });
-
+*/
+//Format connString
+//'mysql://myuser:mypass@myhost/mdb?charset=utf8_general_ci&timezone=-0700';
+var connString = 'mysql://root:@localhost/londri?charset=utf8_general_ci&timezone=-0700';
+ 
+var con = mysql.createConnection(connString);
 con.connect(function (err){
     if(err) throw err;
 });
+
 
 module.exports = con;
